dfaa694852c7585a0824111d1b5651b2827fe709
moksleivis <paparama>
2018-02-27 11:31:54 +0200

    first addition to git. lolololololololololol

---
+++ app/src/main/java/com/byethost4/itisarndwebsite/elitelistapp/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.byethost4.itisarndwebsite.elitelistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);// tevo vykdomas metodo kodas
+        setContentView(R.layout.activity_login);
+    }
+
+
+}

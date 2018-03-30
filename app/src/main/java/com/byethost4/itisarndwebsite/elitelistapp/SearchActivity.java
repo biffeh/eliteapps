d525871f6b3c1ab66a494f84b33cc298ff1ecaa6
moksleivis <paparama>
2018-03-30 11:23:20 +0300

    dar neveikianti auto reg funkcija

--- app/src/main/java/com/byethost4/itisarndwebsite/elitelistapp/SearchActivity.java
+++ app/src/main/java/com/byethost4/itisarndwebsite/elitelistapp/SearchActivity.java
@@ -1,7 +1,10 @@
 package com.byethost4.itisarndwebsite.elitelistapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,6 +12,20 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        Button newEntryButton = findViewById(R.id.new_entry_btn);
+        newEntryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent goToNewEntry = new Intent(SearchActivity.this, EntryRegistryActivity.class);//Pirmas param = is kurios veiklos, Antras į kuria veiklą
+                startActivity(goToNewEntry);
+
+
+            }
+
+
+        });
 
     }
 }
